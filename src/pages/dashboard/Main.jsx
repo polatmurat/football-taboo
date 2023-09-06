6d1c9cfb07aa34ccb854aeb954b66df9f6256a81
Murat Polat <bomch4nte@gmail.com>
2023-09-06 17:36:37 +0300

    dash changes

--- src/pages/dashboard/Main.jsx
+++ src/pages/dashboard/Main.jsx
@@ -48,7 +48,6 @@
 
 // export default Main;
 
-
 import { useEffect, useState } from "react";
 import { FaRegCirclePlay } from "react-icons/fa6";
 import PreStartDash from "../../components/dashboard/PreStartDash";
@@ -61,7 +60,7 @@ const Main = () => {
 
   useEffect(() => {
     if (!start) {
-      navigate('/reg-team');
+      navigate("/reg-team");
     }
   }, [start, navigate]);
 
@@ -69,7 +68,7 @@ const Main = () => {
     <>
       <div className="start-icon w-full flex flex-col justify-center items-center p-20">
         <div className="border-b-2 text-center">
-          <p className="text-md">
+          <p className="text-md my-5">
             Welcome to the Football Taboo App! Get ready to test your knowledge
             and creativity in this exciting word-guessing game. Whether you're a
             die-hard football fan or just looking for some fun, our app
@@ -79,18 +78,21 @@ const Main = () => {
             their football expertise without saying the forbidden words? It's
             time to huddle up and kick off the ultimate taboo experience!
           </p>
-          <p className="my-5 font-medium">
-            Are you up for the challenge? Press the 'Start' button to dive into
-            the world of football taboo and prove your skills at conveying the
-            unspoken!
-          </p>
         </div>
+
         <div
           onClick={() => setStart(!start)}
           className="mt-4 flex items-center justify-center rounded-md py-2 px-9 border-y-2 cursor-pointer hover:border-2 hover:transition-all ease-in-out hover:duration-500 transition-all duration-75"
         >
           <span className="font-medium text-4xl mr-2 mb-2">Start</span>
           <FaRegCirclePlay size={40} />
+        </div>
+        <div className="border-b-2 text-center">
+          <p className="my-5 font-medium">
+            Are you up for the challenge? Press the 'Start' button to dive into
+            the world of football taboo and prove your skills at conveying the
+            unspoken!
+          </p>
         </div>
       </div>
     </>
